e93dc3d4828092cf1e9ff1a9ef55653585ed17d1
516B <118240720+516B@users.noreply.github.com>
2023-11-29 23:51:10 +0900

    회원가입, 로그인 구현
    나머지 기능 구현할 떄 귀찮으니까 로그인 관련 layout, .java는 나중에 적용하는것을 추천드립니다.

--- CreateAccountActivity.java
+++ CreateAccountActivity.java
@@ -1,28 +1,109 @@
 package pack.mp_team5project;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.Calendar;
+
 
 public class CreateAccountActivity extends AppCompatActivity {
 
-    android.widget.Button createdAccount_Btn;
+    android.widget.Button createdAccount_Btn, checkedDuplicateID_Btn;
+    EditText et_register_email, et_register_pw, et_register_repw;
+    private FirebaseAuth mAuth;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_account);
 
-        createdAccount_Btn = (android.widget.Button) findViewById(R.id.createAccount_btn);
+        FirebaseApp.initializeApp(this);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        createdAccount_Btn = findViewById(R.id.createAccount_btn);
+        et_register_email = findViewById(R.id.create_IDEmail);
+        et_register_pw = findViewById(R.id.create_password);
+        et_register_repw = findViewById(R.id.check_password);
 
         createdAccount_Btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intentToCreatedAccount = new Intent(CreateAccountActivity.this, LoginActivity.class);
-                startActivity(intentToCreatedAccount);
+                final String email = et_register_email.getText().toString().trim();
+                final String password = et_register_pw.getText().toString().trim();
+                final String rePassword = et_register_repw.getText().toString().trim();
+
+                if (!email.isEmpty() && !password.isEmpty() && !rePassword.isEmpty()) {
+                    if (!rePassword.equals(password)) {
+                        Toast.makeText(CreateAccountActivity.this, "비밀번호가 서로 일치하지 않습니다.", Toast.LENGTH_SHORT).show();
+                    } else{
+                        registerUser(email, password);
+                    }
+                } else {
+                    Toast.makeText(CreateAccountActivity.this, "모든 항목을 작성해주세요", Toast.LENGTH_SHORT).show();
+                }
             }
         });
     }
+    private void registerUser(String email, String password) {
+                mAuth.createUserWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(CreateAccountActivity.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    // 회원등록 성공시
+                                    FirebaseUser user = mAuth.getCurrentUser();
+
+                                    if (user != null) {
+                                        Toast.makeText(CreateAccountActivity.this, "회원 가입 성공!", Toast.LENGTH_SHORT).show();
+                                        Intent intent = new Intent(CreateAccountActivity.this, LoginActivity.class);
+                                        startActivity(intent);
+                                        finish();
+                                    } else {
+                                        Toast.makeText(CreateAccountActivity.this, "에러 발생", Toast.LENGTH_SHORT).show();
+                                    }
+                                } else {
+                                    // 회원가입 실패
+                                    handleRegistrationError(task.getException());
+                                }
+                            }
+                        });
+            }
+
+    private void handleRegistrationError(Exception e) {
+                if (e != null) {
+                    String errorMessage = "";
+                    if (e.getMessage() != null) {
+                        if (e.getMessage().contains("WEAK_PASSWORD")) {
+                            errorMessage += "Weak password. Please choose a stronger password.";
+                        } else if (e.getMessage().contains("INVALID_EMAIL")) {
+                            errorMessage += "Invalid email format. Please enter a valid email address.";
+                        } else {
+                            errorMessage += e.getMessage();
+                        }
+                    } else {
+                        errorMessage += "Unknown error!";
+                    }
+
+                    Toast.makeText(CreateAccountActivity.this, errorMessage, Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(CreateAccountActivity.this, "Registration failed: Unknown error!", Toast.LENGTH_SHORT).show();
+                }
+            }
 }
--- LoginActivity.java
+++ LoginActivity.java
@@ -1,34 +1,52 @@
 package pack.mp_team5project;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 import android.widget.Button;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.FirebaseApp;
 
 public class LoginActivity extends AppCompatActivity {
 
-
-//    Intent intent = new Intent(SplashActivity.this, LoginActivity.class);
-//    startActivity(intent);
-//
-//    finish();
-
     android.widget.Button login_Btn, createAccount_Btn;
+    EditText et_login_emailID, et_login_password;
+    private FirebaseAuth mAuth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        FirebaseApp.initializeApp(this);
+
+        mAuth = FirebaseAuth.getInstance();
 
         login_Btn = (android.widget.Button) findViewById(R.id.login_btn);
+
+        et_login_emailID = findViewById(R.id.login_emailId);
+        et_login_password = findViewById(R.id.login_password);
 
         login_Btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intentToMain = new Intent(LoginActivity.this, MainActivity.class);
-                startActivity(intentToMain);
+                final String emailID = et_login_emailID.getText().toString().trim();
+                final String password = et_login_password.getText().toString().trim();
+                if (!emailID.isEmpty() && !password.isEmpty()) {
+                    loginUser(emailID, password);
+                } else {
+                    Toast.makeText(LoginActivity.this, "모든 항목을 작성해주세요", Toast.LENGTH_SHORT).show();
+                }
             }
         });
 
@@ -42,4 +60,49 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
     }
+
+    private void loginUser(String email, String password) {
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // 로그인 성공
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            if (user != null) {
+                                Toast.makeText(LoginActivity.this, "로그인 성공!", Toast.LENGTH_SHORT).show();
+                                Intent intentToMain = new Intent(LoginActivity.this, MainActivity.class);
+                                startActivity(intentToMain);
+                            } else {
+                                Toast.makeText(LoginActivity.this, "에러 발생", Toast.LENGTH_SHORT).show();
+                            }
+                        } else {
+                            // 로그인 실패
+                            handleLoginError(task.getException());
+                        }
+                    }
+                });
+    }
+
+    private void handleLoginError(Exception e) {
+        if (e != null) {
+            String errorMessage = "";
+            if (e.getMessage() != null) {
+                if (e.getMessage().contains("INVALID_EMAIL")) {
+                    errorMessage += "Invalid email format. Please enter a valid email address.";
+                } else if (e.getMessage().contains("USER_NOT_FOUND") || e.getMessage().contains("WRONG_PASSWORD")) {
+                    errorMessage += "Invalid email or password. Please check your credentials and try again.";
+                } else {
+                    errorMessage += e.getMessage();
+                }
+            } else {
+                errorMessage += "Unknown error!";
+            }
+
+            Toast.makeText(LoginActivity.this, errorMessage, Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(LoginActivity.this, "Login failed: Unknown error!", Toast.LENGTH_SHORT).show();
+        }
+    }
+
 }
