98b2981f8969ee840dd831babca2267dcf3af684
516B <118240720+516B@users.noreply.github.com>
2023-11-29 23:56:06 +0900

    firebase연결 관련 파일입니다.
    파이어베이스 통합 파이어베이스 사이트에서 소영님이 Authentication생성 후,  google-services.json 다운받고 해당 파일로 교체하면 될 것 같습니다.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,42 +1,10 @@
+buildscript {
+    dependencies {
+        classpath("com.google.gms:google-services:4.4.0")
+    }
+}
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application")
-    id("com.google.gms.google-services")
-}
-
-android {
-    namespace = "pack.mp_team5project"
-    compileSdk = 34
-
-    defaultConfig {
-        applicationId = "pack.mp_team5project"
-        minSdk = 33
-        targetSdk = 33
-        versionCode = 1
-        versionName = "1.0"
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
-        }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
-    }
-}
-
-dependencies {
-
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.10.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("com.google.firebase:firebase-auth:22.3.0")
-    implementation("com.google.firebase:firebase-database-ktx:20.3.0")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    id("com.android.application") version "8.1.1" apply false
+    id("com.android.library") version "8.1.1" apply false
 }
